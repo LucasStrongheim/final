a848da5420c7bfcee775d5d3b2c39cb8bc2462f9
LucasStrongheim <102640172+LucasStrongheim@users.noreply.github.com>
2023-05-05 18:52:35 -0500

    Caloric intake feature
    removed default page HTML and added caloric intake, with BMI and reccomened caloric intake calculators.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CaloricIntakeComponent } from './caloric-intake/caloric-intake.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaloricIntakeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/caloric-intake/caloric-intake.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caloric-intake',
+  templateUrl: './caloric-intake.component.html',
+  styleUrls: ['./caloric-intake.component.css']
+})
+export class CaloricIntakeComponent implements OnInit {
+  // Define variables to hold user input
+  age: number = 0;
+  height: number = 0;
+  weight: number = 0;
+  // Define variables to hold calculated values
+  bmi: number = 0;
+  caloricIntake: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // Calculate BMR based on user input
+  calculateCaloricIntake(gender: string, age: number, height: number, weight: number, activityLevel: number) {
+    // Calculate BMR based on gender, age, height, and weight
+    let bmi: number;
+    if (gender === 'male') {
+      bmi = 88.4 + (13.4 * weight) + (4.8 * height) - (5.68 * age);
+    } else {
+      bmi = 447.6 + (9.25 * weight) + (3.10 * height) - (4.33 * age);
+    }
+
+//caloric intake is based off age
+  calculateCaloricIntake(); {
+    if (age < 20){
+      this.caloricIntake = "1,800 - 2,400 calories";
+        }else if(this.age <40){
+      this.caloricIntake = "1,800 - 2,200 calories";
+        }else if(this.age <50){
+      this.caloricIntake = "1,600 - 2,200 calories";
+        }else{
+      this.caloricIntake = "1,600 - 2,000 calories";
+    }
+    ;
+  }
+
+  // Handle form submission
+  onsubmit() {
+    // Call calculateBMR and calculateCaloricIntake methods
+    this.calculateBMR();
+    this.calculateCaloricIntake();
+  }
+
+}}
