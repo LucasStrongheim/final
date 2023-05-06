8f2107f33e40c0f770cba653399ad29a4d9d4301
LucasStrongheim <102640172+LucasStrongheim@users.noreply.github.com>
2023-05-05 20:36:19 -0500

    caloric-intake edits
    caloric intake component edtis

--- src/app/caloric-intake/caloric-intake.component.ts
+++ src/app/caloric-intake/caloric-intake.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./caloric-intake.component.css']
 })
 export class CaloricIntakeComponent implements OnInit {
-  // Define variables to hold user input
+ 
   age: number = 0;
   height: number = 0;
   weight: number = 0;
-  // Define variables to hold calculated values
+
   bmi: number = 0;
   caloricIntake: string = '';
 
@@ -29,24 +29,16 @@ export class CaloricIntakeComponent implements OnInit {
       bmi = 447.6 + (9.25 * weight) + (3.10 * height) - (4.33 * age);
     }
 
-//caloric intake is based off age
-  calculateCaloricIntake(age, height, weight, activityLevel); {
+  }
+  calculateCaloricIntake(age: number) {
     if (age < 20){
       this.caloricIntake = "1,800 - 2,400 calories";
-        }else if(this.age <40){
+        }else if(age <40){
       this.caloricIntake = "1,800 - 2,200 calories";
         }else if(this.age <50){
       this.caloricIntake = "1,600 - 2,200 calories";
         }else{
       this.caloricIntake = "1,600 - 2,000 calories";
     }
-    ;
   }
-
- 
-
-}}
-function calculateCaloricIntake(age: number, height: number, weight: number, activityLevel: number) {
-  throw new Error('Function not implemented.');
 }
-
