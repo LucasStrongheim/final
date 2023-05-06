e85c2a31b08c8859fcfef58ddeb9dde67c0c26ea
LucasStrongheim <102640172+LucasStrongheim@users.noreply.github.com>
2023-05-05 19:14:18 -0500

    routing + video
    added the video component and added routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { VideoComponent } from './video/video.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'video', component: VideoComponent },
+  { path: 'test', component: AppComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CaloricIntakeComponent } from './caloric-intake/caloric-intake.component';
+import { VideoComponent } from './video/video.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CaloricIntakeComponent
+    CaloricIntakeComponent,
+    VideoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
